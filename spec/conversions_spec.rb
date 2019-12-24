26b00f27f673e97ef0474298612aa425766989a6
Russell Macalintal <russell.macalintal@gmail.com>
2019-12-24 05:45:38 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -6,5 +6,18 @@ describe "conversions" do
       grams = ounces_to_grams(0)
       expect(grams).to eq(0.0)
     end
+    it 'given 1, returns 28.3495' do
+      grams = ounces_to_grams(1)
+      expect(grams).to eq(28.3495)
+    end
   end
+end
+
+
+puts "Given 1, it should return 28.3495"
+answer = ounces_to_grams(1)
+if answer == 28.3495
+  puts "Passed!"
+else
+  puts "Test failed: got #{answer} instead"
 end
