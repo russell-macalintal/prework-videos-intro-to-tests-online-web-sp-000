70d55b6dfa1fbf8b4df783b0b4d0977bc72ecde1
Russell Macalintal <russell.macalintal@gmail.com>
2019-12-24 05:45:46 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -12,12 +12,3 @@ describe "conversions" do
     end
   end
 end
-
-
-puts "Given 1, it should return 28.3495"
-answer = ounces_to_grams(1)
-if answer == 28.3495
-  puts "Passed!"
-else
-  puts "Test failed: got #{answer} instead"
-end
